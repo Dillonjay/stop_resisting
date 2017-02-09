d936041c175cf6356b968f3bcaf862c0786c97da
Dillon Lundell <dillonjaylundell@gmail.com>
2017-02-08 21:23:41 -0600

    list component complete

--- app/components/sahdude.js
+++ app/components/sahdude.js
@@ -4,28 +4,41 @@ export default class App extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			names: []
+			names: [],
+			name: ''
 		}
 	}
 	render() {
 		return (
 			<div className="main">
-				<h1>HELLO!</h1>
+				<ul>
+					{
+						this.state.names.map(function(name,i){
+							return <li key={i}>{ name }... Sah Dude!!</li>
+						})
+					}
+				</ul>
+				<form onSubmit={this.addFriend.bind(this)}>
+              		 <input type="text" value={this.state.name} onChange={this.handleChange.bind(this)} />
+              		<button type="submit">Add A friend!</button>
+              		<button onClick={this.delete.bind(this)}>Remove!</button>
+            	</form>
 			</div>
 		)
 	}
-	// addFriend(e){
- //  		// var names = this.state.items;
- //   	// 	names.push(this.state.input)
-    	
- 
- //  		// this.setState({
- //    // 		items: itemArray
- //  		// });
- 
- //  		// this._inputElement.value = "";
- 
- //  		// e.preventDefault();
-	// }
+	handleChange(event) {
+    	this.setState({name: event.target.value});
+  	}
+  	delete(e) {
+  		let names = this.state.names;
+  		names.pop();
+  		this.setState({ names });
+  		e.preventDefault();
+  	}
+	addFriend(e){
+  		let names = this.state.names;
+   		names.push(this.state.name);
+  		this.setState({ names, name:'' });
+  		e.preventDefault();
+	}
 }
-module.exports = App
